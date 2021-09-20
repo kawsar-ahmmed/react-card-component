22a287c70561387e9bdbf96db9492cf7beafcc52
kawsar91221 <designer.kawsar2018@gmail.com>
2021-09-21 04:23:10 +0600

    end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 
+const myName = 'kawsar'
+// variables 
+
+const cinemas = [
+  {nayok : "Razzak", nayka : "Kobori"},
+  {nayok : "Shakib", nayka : "Opu"},
+  {nayok : "Arifin Shuvo", nayka : "Porimoni"}
+]
+
+// Component
+function Cenima (props){
+  return (
+    <div className="App person-app">
+      <h3>Nayok Name: {props.nayok} </h3>
+      <h4>Nayka Name: {props.nayka} </h4>
+      <a href="">Clieck here</a>
+    </div>
+  )
+}
+
+// Component value
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div className="App app">
+     {
+       cinemas.map (cinema => <Cenima nayok = {cinema.nayok} nayka = {cinema.nayka} ></Cenima>)
+     }
     </div>
   );
 }
 
 export default App;
+
+
